9aef8e53fd65739e00b35167f041455229b70f7e
bufer99 <bufer99@gmail.com>
2023-02-03 18:26:01 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.sass'
+
+function App() {
+
+  return (
+    <div className='wrapper'>
+      <div className='inner'>
+        <div className='flex'>
+          <div className='item'>
+            <div className='item-content'>
+              <img src="https://eu-wotp.wgcdn.co/dcont/fb/image/600x600_water_camouflage_(id_631).png"></img>
+              <div className='text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda placeat sit facilis cupiditate aliquid neque ipsum, asperiores accusantium soluta, perferendis necessitatibus error officiis atque laudantium obcaecati illo saepe quaerat nemo.</div>
+            </div>
+          </div>
+          <div className='item'>
+            <div className='item-content'>
+              <img src="https://eu-wotp.wgcdn.co/dcont/fb/image/600x600_fire_camouflage_(id_630).png"></img>
+              <div className='text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda placeat sit facilis cupiditate aliquid neque ipsum, asperiores accusantium soluta, perferendis necessitatibus error officiis atque laudantium obcaecati illo saepe quaerat nemo.</div>
+            </div>
+          </div>
+          <div className='item'>
+            <div className='item-content'>
+              <img src="https://eu-wotp.wgcdn.co/dcont/fb/image/600x600_earth_camouflage_(id_628).png"></img>
+              <div className='text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda placeat sit facilis cupiditate aliquid neque ipsum, asperiores accusantium soluta, perferendis necessitatibus error officiis atque laudantium obcaecati illo saepe quaerat nemo.</div>
+            </div>
+          </div>
+          <div className='item'>
+            <div className='item-content'>
+              <img src="https://eu-wotp.wgcdn.co/dcont/fb/image/600x600_wood_camouflage_(id_632).png"></img>
+              <div className='text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda placeat sit facilis cupiditate aliquid neque ipsum, asperiores accusantium soluta, perferendis necessitatibus error officiis atque laudantium obcaecati illo saepe quaerat nemo.</div>
+            </div>
+          </div>
+          <div className='item'>
+            <div className='item-content'>
+              <img src="https://eu-wotp.wgcdn.co/dcont/fb/image/600x600_metal_camouflage_(id_629).png"></img>
+              <div className='text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda placeat sit facilis cupiditate aliquid neque ipsum, asperiores accusantium soluta, perferendis necessitatibus error officiis atque laudantium obcaecati illo saepe quaerat nemo.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
